2672bfa84c03dc5b12720a86cfa3fd8d18dd53cd
Анна Береснева <4470984@gmail.com>
2024-12-11 15:47:24 +0500

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,13 @@
+username = "Maxim Maksimov"
+print("Имя пользователя:",username)
+title = "Встреча с клиентом"
+print("Заголовок заметки:", title)
+content = "Коммерческое предложение о покупке мотора"
+print("Описание заметки:",content)
+status = "Подписание договора"
+print("Cтатус заметки:",status)
+created_date = "10-12-2024"
+print("Дата создания заметки:",created_date)
+issue_date = "25-12-2024"
+print("дата истечения заметки:",issue_date)
+
